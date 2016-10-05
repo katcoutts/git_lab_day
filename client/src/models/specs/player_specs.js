9a0bbf9cb83fc3fdb955c8d66e4f9b17582f647f
Katrina Coutts <katcoutts@hotmail.co.uk>
2016-10-05 12:05:50 +0100

    tested player model name

---
+++ client/src/models/specs/player_specs.js
@@ -0,0 +1,20 @@
+var Player = require('../player');
+var assert = require('assert');
+
+describe('Player', function(){
+
+  var player;
+
+  before(function(){
+    player = new Player({
+      name: "Gadwin Springer",
+      position: "Prop",
+      height: 191
+    });
+  });
+
+  it('should have a name', function(){
+    assert.equal("Gadwin Springer", player.name);
+  })
+
+});
