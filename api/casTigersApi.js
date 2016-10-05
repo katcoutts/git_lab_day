ea8fbcdce24741bd2d57bc310a40cc8bd9401adf
Katrina Coutts <katcoutts@hotmail.co.uk>
2016-10-05 11:43:37 +0100

    data hardcoded to api

---
+++ api/casTigersApi.js
@@ -0,0 +1,34 @@
+var CasTigersApi = function(app){
+
+  var casTigers = [{
+    name: "Luke Dorn",
+    position: "Full Back",
+    height: 175
+  },
+  {
+    name: "Jake Webster",
+    position: "Centre",
+    height: 182
+  },
+  {
+    name: "Denny Solomona",
+    position: "Winger",
+    height: 190
+  },
+  {
+    name: "Michael Shenton",
+    position: "Centre",
+    height: 190
+  },
+  {
+    name: "Gadwin Springer",
+    position: "Prop",
+    height: 191
+  }
+
+
+
+  ]
+  
+
+}
