e836700138db3c6f3870cebb7c8f831fd72c7fa4
DavidMcKenzie218 <davidmckenzie218@gmail.com>
2016-10-05 11:54:25 +0100

    Added the api to the server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+var casTigersApi = require('./api/casTigersApi.js')
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
@@ -11,6 +13,8 @@ app.use(express.static('client/build'));
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
+
+  new casTigersApi(app);
 
   console.log('app listening at http://%s:%s', host, port);
 });
